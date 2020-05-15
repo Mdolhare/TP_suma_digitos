328d0092365572b72ca0fcd960e4c0e636f21026
mdolhare <mdolhare@itba.edu.ar>
2020-05-15 12:02:57 -0300

    main que no funciona =)

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define NUM 294
+
+int suma_digitos(int n1);
+
+int main()
+{
+	int c = suma_digitos (NUM);
+	return 0;
+}
+
+int suma_digitos(int n1)
+{
+	return((n1%10)+digitos(n1/10)); //caso recursivo
+}
