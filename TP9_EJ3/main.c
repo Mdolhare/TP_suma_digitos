166d4e7b06c0f5d37b0c107fc99cc42d092f0286
MateoLucas <mlucas@itba.edu.ar>
2020-05-17 21:30:41 -0300

    Transformado a proyecto 2.1

--- TP9_EJ3/main.c
+++ TP9_EJ3/main.c
@@ -10,7 +10,14 @@
  *
  * Created on May 16, 2020, 3:27 PM
  */
+/*
+a. El objetivo de la funcion es sumar los digitos que componen un numero. Ej: 12 -> 1 + 2.
+b. No funciona por dos razones. Primero que en el return se llamaba a la funcion "digitos" y no "suma_digitos".
+Y segundo porque nunca dejaba de llamar a la funcion para devolver algun valor.
 
+
+
+*/
 #include <stdio.h>
 #include "numero.h"
 #include "funcion.h"
